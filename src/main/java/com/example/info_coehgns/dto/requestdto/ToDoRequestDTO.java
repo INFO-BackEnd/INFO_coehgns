2e87ce39d0ff6185f3b15553703d6247e64c7fce
도훈 <modooboiroo@gmail.com>
2024-06-17 22:17:48 +0900

    feat :: RequestDTO에 @NotBlank를 사용하여 필드 값이 비어있지 않도록 검증

--- src/main/java/com/example/info_coehgns/dto/requestdto/ToDoRequestDTO.java
+++ src/main/java/com/example/info_coehgns/dto/requestdto/ToDoRequestDTO.java
@@ -1,9 +1,12 @@
 package com.example.info_coehgns.dto.requestdto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ToDoRequestDTO {
+    @NotBlank
     private String content;
+    @NotBlank
     private boolean isDone;
 }
--- src/main/java/com/example/info_coehgns/dto/requestdto/UpdateToDoRequestDTO.java
+++ src/main/java/com/example/info_coehgns/dto/requestdto/UpdateToDoRequestDTO.java
@@ -1,10 +1,12 @@
 package com.example.info_coehgns.dto.requestdto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 
 @Getter
 @Data
 public class UpdateToDoRequestDTO {
+    @NotBlank
     private boolean isDone;
 }
